5e19a944af565d391542351622d82e91395b204d
Lewis Harvey <lewisjharvey@gmail.com>
2013-07-02 09:31:30 +0100

    Adding application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Easemyeyes::Application.config.secret_key_base = 'c0b98aeefcaa168ae93754bbbbfa39e0e7a9a7e2a339e266406c7d25f872ccaee0b37cb6ef7cc74d52c9f51d71c4ce01977f1e928b669e7f1b618ca85beadc93'
